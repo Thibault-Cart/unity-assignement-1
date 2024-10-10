f3beb114fd51e520a51efbd4a8bfdf5614fc71ff
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2024-10-10 19:04:29 +0900

    assignement 3 last commit

---
+++ Assets/DuckScript.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptTest : MonoBehaviour
+{
+    public float speed = 0.2f;
+    public float lifeTime = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+
+
+
+
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+
+        if (lifeTime <= 0.0f)
+        {
+
+            Destroy(gameObject);
+        }
+
+        this.transform.Translate(0, -speed * Time.deltaTime, 0);
+
+    }
+}
