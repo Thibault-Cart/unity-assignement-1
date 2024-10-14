e0164a4e5bbb58aedf49f4fcb0aa5002f22c2924
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2024-10-14 18:05:16 +0900

    Reapply "game over made"
    This reverts commit a369ed4f4ac4ed01ddacc2b49d7ea3ba107806e5.

---
+++ Assets/ScriptQuitGame.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScriptQuitGame : MonoBehaviour
+{
+    public Button btnRestart;
+    public Button btnQuit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn1 = btnRestart.GetComponent<Button>();
+        btn1.onClick.AddListener(btnRestartClick);
+        Button btn2 = btnQuit.GetComponent<Button>();
+        btn2.onClick.AddListener(btnQuitClick);
+
+    }
+
+    private void btnQuitClick()
+    {
+        print("app quit");
+        Application.Quit();
+    }
+
+    private void btnRestartClick()
+    {
+        print("switch scene");
+
+        SceneManager.LoadScene("MainScene");  // Replace with your scene name
+
+    }
+
+    public void test()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
