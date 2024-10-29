280d6d7ff979a81c5eccff021f59a0e0a203811b
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2024-10-29 16:31:52 +0900

    pathfinding added

--- Assets/DuckScript.cs
+++ Assets/DuckScript.cs
@@ -1,16 +1,20 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class scriptTest : MonoBehaviour
 {
     public float speed = 0.2f;
     public float lifeTime = 20;
+    private GameObject objectiv;
 
+    public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-
+        objectiv = GameObject.Find("Base");
+        agent = GetComponent<NavMeshAgent>();
     }
     
     
@@ -21,6 +25,10 @@ public class scriptTest : MonoBehaviour
 
     void Update()
     {
+        print(objectiv.transform);
+
+        agent.destination=objectiv.transform.position;
+        /*
         lifeTime -= Time.deltaTime;
 
         if (lifeTime <= 0.0f)
@@ -30,6 +38,6 @@ public class scriptTest : MonoBehaviour
         }
 
         this.transform.Translate(0, -speed * Time.deltaTime, 0);
-
+        */
     }
 }
